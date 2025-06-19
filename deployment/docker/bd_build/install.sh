b920938fd2c75675cd42df02f62669501009f2c5
zhangjun <ibmzhangjun@139.com>
2025-06-19 22:40:37 +0800

    updatesql

--- deployment/docker/bd_build/install.sh
+++ deployment/docker/bd_build/install.sh
@@ -3,7 +3,7 @@ set -e
 set -x
 apt-get update && DEBIAN_FRONTEND=noninteractive && \
 apt -y dist-upgrade && \
-apt-get install -y --no-install-recommends build-essential libssl-dev libffi-dev python3-dev net-tools libsasl2-dev cmake libsnappy-dev zlib1g-dev libbz2-dev libgflags-dev liblz4-dev curl wget procps netcat git libnss3-tools python3-pip && \
+apt-get install -y --no-install-recommends build-essential libssl-dev libffi-dev python3-dev net-tools libsasl2-dev libsnappy-dev cmake libsnappy-dev zlib1g-dev libbz2-dev libgflags-dev liblz4-dev curl wget procps netcat git libnss3-tools python3-pip && \
 pip3 install virtualenv && \
 cd /opt && \
 git clone https://github.com/zinohome/MACDA-SZ16.git && \
@@ -14,6 +14,7 @@ mkdir -p /opt/MACDA/log && \
 virtualenv venv && \
 . venv/bin/activate && \
 pip3 install -r requirements.txt && \
+pip3 install 'aiokafka[snappy]' && \
 cd /opt/MACDA && cp /bd_build/default_env /opt/MACDA/.env && \
 cp /bd_build/wait-for /usr/bin/wait-for && chmod 755 /usr/bin/wait-for && \
 ls -l /opt/MACDA/.env && cat /opt/MACDA/.env && \
